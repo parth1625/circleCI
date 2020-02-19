e2486d0a0b125775b018ee615d9780addfe3c9e7
Parth Vijay <parth@securitytroops.com>
2020-02-19 16:54:27 +0530

    testing master

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-# Create your views here.
 
+# Create your views here.
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
